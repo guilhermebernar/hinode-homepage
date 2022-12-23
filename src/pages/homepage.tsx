041f40e38a9f50c14f9a54dcf30d5a7111ee7f99
Guilherme Bernardo <guilhermebernardo@id.uff.br>
2022-12-23 19:04:53 -0300

    feat: beggining homepage html

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,6 +1,87 @@
 const HomePage = ()=> {
     return (
-        <h1>OK</h1>
+        <>
+            <main>
+                <header>
+                    <section>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </section>
+                    <section>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </section>
+                    <section>
+                        <div></div>
+                        <div></div>
+                    </section>
+                </header>
+                <section>   
+                
+                    {/* CARROCEL PRINCIPAL */}
+                    <section>
+                    </section>
+                
+                    {/* Caracteristicas */}
+                    <section>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </section>
+
+                    {/* Mais vendidos */}
+                    <section>
+
+                    </section>
+                    
+                    {/* Isso pode ser seu interesse */}
+                    <section>
+
+                    </section>
+
+                    {/* Os mais desejados */}
+                    <section>
+
+                    </section>
+
+                    {/* Vida saudável */}
+                    <section>
+
+                    </section>
+
+                    {/* Corpo e banho */}
+                    <section>
+
+                    </section>
+
+                    {/* Fragâncias */}
+                    <section>
+
+                    </section>
+
+                    {/* Acabaram de chegar */}
+                    <section>
+
+                    </section>
+
+                </section>
+
+                <footer>
+                    {/* Receba nossas ofertas e novidades */}
+                    <section>
+
+                    </section>
+
+                    {/* Informações gerais */}
+                    <section>
+
+                    </section>
+                </footer>
+            </main>
+        </>
     );
   }
   
